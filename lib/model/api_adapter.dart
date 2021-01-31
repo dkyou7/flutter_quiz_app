fe111e3653bb824654a950f0907d052bd7d9e4f4
youdongkwan <dkyou7@naver.com>
2021-01-31 19:46:33 +0900

    api_adapter.dart 파일 작성

---
+++ lib/model/api_adapter.dart
@@ -0,0 +1,7 @@
+import 'model_quiz.dart';
+import 'dart:convert';
+
+List<Quiz> parseQuizs(String responseBody){
+  final parsed = json.decode(responseBody).cast<Map<String,dynamic>>();
+  return parsed.map<Quiz>((json) => Quiz.fromJson(json)).toList();
+}
